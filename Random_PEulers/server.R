b8cef60ae2436bf062cdc3fbcd147901462b8d96
Chris J Crichton <chrisjcrichton@hotmail.com>
2016-01-29 23:06:10 -0500

    ui laid out...think it'll work...

--- Random_PEulers/server.R
+++ Random_PEulers/server.R
@@ -1,22 +1,29 @@
 
+
 library(shiny)
 library(shinyjs)
 
+ProjEul1 <- function(x) {
+  
+  
+  
+  
+  return()
+}
+
 
 shinyServer(function(input, output, session) {
-
-    observe({
-      shinyjs::toggle(id = "Ex1")
-    })
+  shinyjs::onclick("Ex1Proc",
+                   shinyjs::toggle(id = "EX1"))
+  shinyjs::onclick("Ex2Proc",
+                   shinyjs::toggle(id = "EX2"))          
+  observeEvent(input$resB, {
+    session$reload();
+  })
   
-    shinyjs::onclick("Ex1Proc",
-                   shinyjs::toggle(id = "Ex1")
-                   )
-
-    observeEvent(input$resB, {
-      session$reload();
-    })
-    
-    
-    
+  observeEvent(input$submit1, {
+    output$results <- renderTable({ProjEul1(input$Ex1N)})
+  })
+  
+ 
 })
--- Random_PEulers/ui.R
+++ Random_PEulers/ui.R
@@ -1,57 +1,62 @@
 
 
+
 library(shiny)
 library(shinyjs)
 
 ######DISCLAIMER: I AM BY NO MEANS A PROFICIENT CSS DESIGNER.##########
 ######        TIPS ARE WELCOME!!!                             #########
 
-shinyUI(fluidPage(
-  
-  ###########TAGS/LINKS#########
-  useShinyjs(),
-  tagList(
-    tags$head(
-      tags$link(rel="stylesheet", type="text/css",href="style.css")
-    ),
-
-
-  ###########PICKER DIV (LEFT "SIDEBAR")####################
-  tags$div(class="picker", id="pick",
-           titlePanel("Project Euler Exercises"),
-           fluidRow(column(12,
-                           a(id="Ex1Proc", "Project Euler 1", href = "#")   
-           )),
-           fluidRow(column(12,
-                           a(id="Ex2Proc", "Project Euler 2", href = "#")   
-           ))       
-           #ONE ROW PER PROBLEM SEEMS TO LOOK OK#
-  ),
-
-  
-      #########DUPLICATE DIVS FOR EACH EX?...##############
-            tags$div(class="Exer", id="Ex1", column(12,
-
-                h3("Find the sum of all the multiples of 3 or 5 below x."),
-                sliderInput("Ex1N", "x = ",
-                            min=1, max=1000, value=1, step=1, round=0
-                ),
-                actionButton("gen1", "Generate"),
-                tags$div(class = "reset",
-                    actionButton("resB", "Reset")
-                ),
-
-                tags$div(class="rslts",
-                      h1("Results"),
-                      tableOutput("results")
-                  )   
-                )
-            )
-
-  ####TAG END    
-  )
-  
-  
-  
-  #####UI END
+shinyUI(fluidPage(###########TAGS/LINKS#########
+                  useShinyjs(),
+                  tagList(
+                    tags$head(
+                      tags$link(rel = "stylesheet", type = "text/css",href = "style.css")
+                    ),
+                    
+                    
+                    ###########PICKER DIV (LEFT "SIDEBAR")####################
+                    tags$div(
+                      class = "picker",
+                      titlePanel("Project Euler Exercises"),
+                      a(class="ExLink", id = "Ex1Proc", "Project Euler 1", href = "#"),
+                      hidden(tags$div(
+                        class = "ExBox", id = "EX1",
+                        fluidRow(column(
+                          6,
+                          h3("Find the sum of all the multiples of 3 or 5 below x."),
+                          sliderInput(
+                            "Ex1N", "x = ",
+                            min = 1, max = 1000, value = 1, step =
+                              1, round = 0
+                          ),
+                          actionButton(class = "sButton", "submit1", "Submit")
+                        ))
+                      )),
+                      a(class="ExLink", id = "Ex2Proc", "Project Euler 2", href = "#"),
+                      hidden(tags$div(
+                        class = "ExBox", id = "EX2",
+                        fluidRow(column(
+                          6,
+                          h3("By considering the terms in the Fibonacci sequence whose values do not x, find the sum of the even-valued terms."),
+                          sliderInput(
+                            "Ex2N", "x = ",
+                            min = 1, max = 4000000, value = 1, step =
+                              1, round = 0
+                          ),
+                          actionButton(class = "sButton", "submit2", "Submit")
+                        ))
+                      ))
+                      
+                      #ONE ROW PER PROBLEM SEEMS TO LOOK OK#
+                    ),
+                    
+                    tags$div(class = "rslts", column(12,
+                                              tableOutput("results")))
+                    
+                    ####TAG END
+                  )
+                  
+                  #####UI END
 ))
+                  
