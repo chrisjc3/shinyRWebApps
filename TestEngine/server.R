34f3e3d03298eaafd46d1e72378c6875a5e1c58a
Chris J Crichton <chrisjcrichton@hotmail.com>
2016-02-07 13:57:40 -0500

    end state implemented, now for feedback and logging

--- TestEngine/server.R
+++ TestEngine/server.R
@@ -6,68 +6,107 @@ library(shiny)
 
 
 shinyServer(function(input, output, session) {
-  #AVAILABLE OUTPUT REACTIVES
   out1 <- reactiveValues()
-  out2 <- reactiveValues()
-  out3 <- reactiveValues()
-  #Recycled counter from MasterMind project
-  turnCtr<-function(x){
-    if(is.na(x)){
+  
+  turnCtr <- function(x) {
+    if (is.na(x)) {
       return(0)
     } else {
-      return(x+1)
+      return(x + 1)
     }
   }
-  #####################1:
-  #Only do any of this when submit button is pushed
-    #start back to question one if submit button is pushed (ie refresh)
+  
   observeEvent(input$startTest,{
-    TurnMax<-input$how_many_questions
-    if(input$startTest == 1){turn <<- 0}
-    turn <<- turnCtr(turn)
-    if(turn <= TurnMax & turn != 0) {
-      out1$out<-list(
-        #If I was going to randomize the order it'd have to be up here
-          #little unecessary
-        src = paste0("www/questions/que_", turn,".png"),
-        width=500,
-        height=250)
+    TurnMax <<- input$how_many_questions
+    if (input$startTest == 1) {
+      turn <<- 0
     }
-    output$question<-renderImage({
-      out1$out
-    },deleteFile=FALSE)
+    turn <<- turnCtr(turn)
+    if (turn <= TurnMax & turn != 0) {
+      out1$out <- list(
+        src = paste0("www/questions/que_", turn,".png"),
+        width = 800,
+        height = 250
+      )
+      output$question <- renderImage({
+        out1$out
+      },deleteFile = FALSE)
+      
+      out1$ansData <- read.xlsx("www/ANSWERS.xlsx",1)
+      out1$ansRow <- subset(out1$ansData, out1$ansData[,1] == turn)
+      out1$corAns <- out1$ansRow[,2]
+      out1$aChoices <- out1$ansRow[1,3:6]
+      
+      updateCheckboxGroupInput(
+        session, "aOptions",
+        choices = list(
+          as.character(out1$aChoices[,1]),
+          as.character(out1$aChoices[,2]),
+          as.character(out1$aChoices[,3]),
+          as.character(out1$aChoices[,4])
+        )
+      )
+    }
     
-    ####################3:
-    #populate checkboxInput buttons 
-    #based on www/ANSWERS.xlsx
-    out1$ansData <- read.xlsx("www/ANSWERS.xlsx",1)
-    out1$ansRow<-subset(out1$ansData, out1$ansData[,1] == turn)
-    out1$corAns<-out1$ansRow[,2]
-    out1$aChoices <- out1$ansRow[1,3:6]
-    
-    # output$test <- renderTable({out1$aChoices})
-    updateCheckboxGroupInput(session, "aOptions",
-                             choices=list(as.character(out1$aChoices[,1]),
-                                          as.character(out1$aChoices[,2]),
-                                          as.character(out1$aChoices[,3]),
-                                          as.character(out1$aChoices[,4])
-                                          ))
   })
   
   observeEvent(input$submitAnswer,{
+    # output$test<-renderText({out1$corAns})
+    ###################4:
+    #log feedback
     
+    
+    
+    #PROCESS NEXT QUESTION:
+    turn <<- turnCtr(turn)
+    if (turn <= TurnMax & turn != 0) {
+      out1$out <- list(
+        src = paste0("www/questions/que_", turn,".png"),
+        width = 800,
+        height = 250
+      )
+      
+      output$question <- renderImage({
+        out1$out
+      },deleteFile = FALSE)
+      
+      out1$ansData <- read.xlsx("www/ANSWERS.xlsx",1)
+      out1$ansRow <- subset(out1$ansData, out1$ansData[,1] == turn)
+      out1$corAns <- out1$ansRow[,2]
+      out1$aChoices <- out1$ansRow[1,3:6]
+      updateCheckboxGroupInput(
+        session, "aOptions",
+        choices = list(
+          as.character(out1$aChoices[,1]),
+          as.character(out1$aChoices[,2]),
+          as.character(out1$aChoices[,3]),
+          as.character(out1$aChoices[,4])
+        )
+      )
+    } else if (turn > TurnMax) {
+      #output results?
+      updateCheckboxGroupInput(session, "aOptions", choices = character(0))
+      out1$out <- list(
+        src = paste0("www/questions/finished.png"),
+        width = 250,
+        height = 250
+      )
+      output$question <- renderImage({
+        out1$out
+      },deleteFile = FALSE)
+      
+    }
   })
   
-
   
-
   
-  ###################4:
-  #log feedback
+  
+  
+  
   
   ###################5:
   #sidebar panel should show instance correct/incorrect counts.
-  #sidebar panel should button to export results 
-    #(or maybe do it automatically when complete?)
+  #sidebar panel should button to export results
+  #(or maybe do it automatically when complete?)
   
 })
--- TestEngine/ui.R
+++ TestEngine/ui.R
@@ -6,26 +6,23 @@ library(shiny)
 library(DT)
 
 shinyUI(fluidPage(
-  
   sidebarLayout(
-    
-    sidebarPanel("Test Options", 
-                 sliderInput(
-                   "how_many_questions", "How many questions?",
-                   min = 5, max = 120, value = 5, step = 1, round = 0),
-                 actionButton("startTest", "Start")),
-    sidebarPanel("Answer Options",
-                 checkboxGroupInput("aOptions", "Options", choices = NULL),
-                 actionButton("submitAnswer","Submit")
-                )       
-    
+    sidebarPanel(
+      "Test Options",
+      sliderInput(
+        "how_many_questions", "How many questions?",
+        min = 5, max = 120, value = 5, step = 1, round = 0
+      ),
+      actionButton("startTest", "Start")
     ),
-    mainPanel("Question Info",
-              imageOutput("question"),
-              tableOutput("test")
-    )    
-  )
-
-  
-  
-)
+    sidebarPanel(
+      "Answer Options",
+      checkboxGroupInput("aOptions", "Options", choices = NULL),
+      actionButton("submitAnswer","Submit")
+    )
+    
+  ),
+  mainPanel("Question Info",
+            imageOutput("question"),
+            textOutput("test"))
+))
