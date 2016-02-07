eb2dfec0f3658db86360c9faff2df2f0cc7bf2e5
Chris J Crichton <chrisjcrichton@hotmail.com>
2016-02-07 15:31:54 -0500

    refresh button...pseudo code for outputs/randomizing order

--- TestEngine/server.R
+++ TestEngine/server.R
@@ -6,8 +6,14 @@ library(shiny)
 
 
 shinyServer(function(input, output, session) {
+  observeEvent(input$refreshTest,{
+    session$reload()
+  })
+  
   out1 <- reactiveValues()
   out2 <- reactiveValues()
+  shuffAnsDB <- reactiveValues()
+  
   turnCtr <- function(x) {
     if (is.na(x)) {
       return(0)
@@ -15,7 +21,10 @@ shinyServer(function(input, output, session) {
       return(x + 1)
     }
   }
+  #MAKE A SHUFFLE FUNCTION FOR ANSWERS.xlsx
+    #CREATE NEW_QNO AND LEAVE QNO ALONE TO CORRELATE TO IMAGE NAMING
   
+  #PERHAPS A SECOND FUNTION SHOULD JUST RETURN the que_# TO REPLACE turn
   observeEvent(input$startTest,{
     TurnMax <<- as.numeric(input$how_many_questions)
     if (input$startTest == 1) {
@@ -49,14 +58,17 @@ shinyServer(function(input, output, session) {
     
   })
   
-
-  
   observeEvent(input$submitAnswer,{
-    out2$newline<-c(turn, as.character(out1$corAns), input$aOptions)
-    out2$feedback<-rbind(out2$feedback, out2$newline)
-    output$test<-renderTable({
-      out2$feedback
-    })
+    if (as.character(out1$corAns) == input$aOptions) {
+      out2$newline <-
+        c(turn, as.character(out1$corAns), input$aOptions, "CORRECT")
+      out2$feedback <- rbind(out2$feedback, out2$newline)
+    } else {
+      out2$newline <-
+        c(turn, as.character(out1$corAns), input$aOptions, "WRONG")
+      out2$feedback <- rbind(out2$feedback, out2$newline)
+    }
+    
     #PROCESS NEXT QUESTION:
     turn <<- turnCtr(turn)
     if (turn <= TurnMax & turn != 0) {
@@ -83,8 +95,17 @@ shinyServer(function(input, output, session) {
           as.character(out1$aChoices[,4])
         )
       )
+      out2$corCt <-
+        subset(out2$feedback , out2$feedback[,4] == "CORRECT")
+      out2$badCt <-
+        subset(out2$feedback , out2$feedback[,4] != "CORRECT")
+      output$Corrects <- renderText({
+        paste(length(out2$corCt[,1]), "/", TurnMax, sep = "")
+      })
+      output$Incorrects <- renderText({
+        paste(length(out2$badCt[,1]), "/", TurnMax, sep = "")
+      })
     } else if (turn > TurnMax) {
-      #output results?
       updateCheckboxGroupInput(session, "aOptions", choices = character(0))
       out1$out <- list(
         src = paste0("www/questions/finished.png"),
@@ -94,23 +115,37 @@ shinyServer(function(input, output, session) {
       output$question <- renderImage({
         out1$out
       },deleteFile = FALSE)
+      out2$corCt <-
+        subset(out2$feedback , out2$feedback[,4] == "CORRECT")
+      out2$badCt <-
+        subset(out2$feedback , out2$feedback[,4] != "CORRECT")
+      output$Corrects <- renderText({
+        paste(length(out2$corCt[,1]), "/", TurnMax, sep = "")
+      })
+      output$Incorrects <- renderText({
+        paste(length(out2$badCt[,1]), "/", TurnMax, sep = "")
+      })
+      out2$fct<-as.numeric(length(out2$corCt[,1]))/as.numeric(TurnMax)
+      output$corPerct <- renderText({
+        as.character(out2$fct)
+      })
       
+      #IT COUNTS CORRECT AND INCORRECT AS IT GOES
+      #ONCE LAST QUESTION INPUT:
+        #Percentage correct in sidebarPanel
+      #in mainPanel
+        #render a "next" button to scroll through table of incorrect answers
+        #each slide should have
+        #Incorrect question qno, correct answer, answer input
+        #& imagine of explanation.png underneath
+      colnames(out2$feedback)<-c("QNO","CORRECT ANSWER","YOUR ANSWER","DEBUG")
+      
+      output$feedback<-renderTable({out2$feedback})
+      
+        
     }
-
-    ###################4:
-    #log feedback
+    
+    
     
   })
-  
-  
-  
-  
-  
-  
-  
-  ###################5:
-  #sidebar panel should show instance correct/incorrect counts.
-  #sidebar panel should button to export results
-  #(or maybe do it automatically when complete?)
-  
 })
--- TestEngine/ui.R
+++ TestEngine/ui.R
@@ -13,16 +13,24 @@ shinyUI(fluidPage(
         "how_many_questions", "How many questions?",
         min = 5, max = 120, value = 5, step = 1, round = 0
       ),
-      actionButton("startTest", "Start")
+      actionButton("startTest", "Start"),
+      actionButton("refreshTest", "Refresh")
     ),
     sidebarPanel(
       "Answer Options",
       checkboxGroupInput("aOptions", "Options", choices = NULL),
-      actionButton("submitAnswer","Submit")
+      actionButton("submitAnswer","Submit"),
+      h6("Correct Counter:"),
+      textOutput("Corrects"),
+      h6("Incorrect Counter:"),
+      textOutput("Incorrects"),
+      h6("Final Score:"),
+      textOutput("corPerct")
     )
     
   ),
   mainPanel("Question Info",
             imageOutput("question"),
-            tableOutput("test"))
+            tableOutput("feedback")
+            )
 ))
